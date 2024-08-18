d394fbe4ab141deb17348440ad197b062a07d6b2
Deeksha Prabhakar <deekiyengar@gmail.com>
2024-08-18 16:53:35 -0700

    chore: nextjs config for static asset

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import LogoComponent from "./logo";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
-      <h1 className="text-6xl font-bold">Coming soon</h1>
-     </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>               
+              <LogoComponent />
+              <footer className="flex items-center justify-center w-full h-24">
+                Coming soon
+                <Image src="/flower.png" alt="Flower image" width={12} height={16} />
+              </footer>
+            </div>
+        </main>
+    );
 }
